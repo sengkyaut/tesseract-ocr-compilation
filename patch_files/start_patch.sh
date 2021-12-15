6a318c65539f07380afed2af0dce54b29c2ad51d
sengkyaut <sengkyaut@hotmail.com>
2021-12-15 06:06:45 +0000

    fix patch path

--- patch_files/start_patch.sh
+++ patch_files/start_patch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-patch ${TESSDATA_PREFIX}/tesstrain_utils.sh -i ${SCRIPTS_DIR}/tesstrain_utils.patch
+patch /usr/share/tesseract-ocr/tesstrain_utils.sh -i ${SCRIPTS_DIR}/tesstrain_utils.patch
 patch ${BASE_DIR}/tesseract/src/training/unicharset/validate_myanmar.cpp -i ${SCRIPTS_DIR}/validate_myanmar.patch
